e937195879a1438b382a2178cb9f78c11148b031
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2022-07-20 16:56:49 +0530

    Permutation in String

---
+++ SlidingWindow/PermutationInString.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/permutation-in-string/
+
+
+def checkInclusion(s1, s2):
+
+    if len(s1) > len(s2):
+        return False
+
+    s1Count, s2Count = [0] * 26, [0] * 26
+
+    for i in range(len(s1)):
+
+        s1Count[ord(s1[i]) - ord("a")] += 1
+        s2Count[ord(s2[i]) - ord("a")] += 1
+
+    matches = 0
+    for i in range(26):
+        if s1Count[i] == s2Count[i]:
+            matches += 1
+
+    l = 0
+    for r in range(len(s1), len(s2)):
+
+        if matches == 26:
+            return True
+
+        index = ord(s2[r]) - ord("a")
+        s2Count[index] += 1
+        if s2Count[index] == s1Count[index]:
+            matches += 1
+        elif s1Count[index] + 1 == s2Count[index]:
+            matches -= 1
+
+        index = ord(s2[l]) - ord("a")
+        s2Count[index] -= 1
+        if s2Count[index] == s1Count[index]:
+            matches += 1
+        elif s1Count[index] - 1 == s2Count[index]:
+            matches -= 1
+        l += 1
+    return matches == 26
+
+
+s1 = "ab"
+s2 = "eidbaooo"
+print(checkInclusion(s1, s2))
