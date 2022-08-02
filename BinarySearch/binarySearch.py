7c18a978e50b7c5dcbc1cd0a65b2f47d5e8f0c48
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2022-08-02 20:38:03 +0530

    Binary Search

---
+++ BinarySearch/binarySearch.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/binary-search/
+
+# Time Complexity: O(log n)
+
+
+def search(nums, target):
+
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+
+        mid = left + (right - left) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
+
+print(search(nums, target))
